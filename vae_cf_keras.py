bf588e00329a8176b895d7ad396c1668fa8e6019
Kilol Gupta <gupta.kilol1@gmail.com>
2018-04-16 00:38:43 -0400

    Update vae_cf_keras.py

--- vae_cf_keras.py
+++ vae_cf_keras.py
@@ -11,7 +11,7 @@ from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
 
 # encoder/decoder network size
 batch_size=500
-original_dim=26744 # number of movies
+original_dim=26621 # number of movies
 intermediate_dim=600
 latent_dim=200
 nb_epochs=50 # to test, otherwise should be 200
@@ -63,21 +63,22 @@ vae.compile(optimizer='adam', loss=vae_loss)
 
 x_train = pickle.load( open( "train_data.file", "rb" ) )
 x_train = x_train[0:118000, :]
-print(x_train.shape[0])
+print("number of training users: ", x_train.shape[0])
 
 x_val = pickle.load( open( "val_data.file", "rb" ) )
 x_val = x_val.todense()
 
-def nn_batch_generator():
-    number_of_batches = x_train.shape[0]/batch_size
+def nn_batch_generator(x, y, batch_size):
+    number_of_batches = x.shape[0]/batch_size
     counter=0
-    index = np.arange(x_train.shape[0])
+    index = np.arange(x.shape[0])
     while 1:
         index_batch = index[batch_size*counter:batch_size*(counter+1)]
-        x_batch = x_train[index_batch,:].todense()
+        x_batch = x[index_batch,:].todense()
+        y_batch = y[index_batch,:].todense()
         counter += 1
-        yield np.array(x_batch),np.array(x_batch)
-        if (counter > number_of_batches):
+        yield (np.array(x_batch),np.array(y_batch))
+        if (counter >= number_of_batches):
             counter=0
 
 
@@ -85,4 +86,4 @@ weightsPath = "./tmp/weights.hdf5"
 checkpointer = ModelCheckpoint(filepath=weightsPath, verbose=1)
 
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.001)
-vae.fit_generator(nn_batch_generator(), samples_per_epoch=x_train.shape[0], nb_epoch=nb_epochs, callbacks=[checkpointer, reduce_lr], validation_data=(x_val, x_val))
+vae.fit_generator(nn_batch_generator(x_train, x_train, batch_size), samples_per_epoch=118000, nb_epoch=nb_epochs, callbacks=[checkpointer, reduce_lr], validation_data=(x_val, x_val))
